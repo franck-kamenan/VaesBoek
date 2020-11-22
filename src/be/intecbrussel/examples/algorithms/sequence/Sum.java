e331e98bc99abea26c60bf7a23d56a3c79d27ade
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-22 10:38:26 +0100

    Sum

---
+++ src/be/intecbrussel/examples/algorithms/sequence/Sum.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.examples.algorithms.sequence;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 6;
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
