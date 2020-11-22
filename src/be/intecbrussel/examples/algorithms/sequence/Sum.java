257fa0503438c0388cd7f37e2fc772f353c2e502
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-22 10:51:17 +0100

    keyboard input

--- src/be/intecbrussel/examples/algorithms/sequence/Sum.java
+++ src/be/intecbrussel/examples/algorithms/sequence/Sum.java
@@ -1,13 +1,21 @@
 package be.intecbrussel.examples.algorithms.sequence;
 
+import java.util.Scanner;
+
 public class Sum {
 
     public static void main(String[] args) {
 
-        int a = 5;
-        int b = 6;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int a = keyboard.nextInt();
+        System.out.println("Enter another number:");
+        int b = keyboard.nextInt();
         int sum = a + b;
 
-        System.out.println(sum);
+        System.out.println("The sum is " + sum);
+
+        keyboard.close();
     }
 }
