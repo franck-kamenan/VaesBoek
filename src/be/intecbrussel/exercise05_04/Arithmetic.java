2ccb7291bd047b4ef8a53976217d998135ee2282
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-23 12:23:30 +0100

    Coditionele operatoren, Ternaire operatoren

--- src/be/intecbrussel/exercise05_04/Arithmetic.java
+++ src/be/intecbrussel/exercise05_04/Arithmetic.java
@@ -7,17 +7,9 @@ public class Arithmetic {
         int value1 = 5;
         int value2 = 3;
 
-        System.out.println(value1 = value2);
-        System.out.println(value1 += value2);
-        System.out.println(value1 -= value2);
-        System.out.println(value1 *= value2);
-        System.out.println(value1 /= value2);
-        System.out.println(value1 %= value2);
-        System.out.println(value1 &= value2);
-        System.out.println(value1 |= value2);
-        System.out.println(value1 ^= value2);
-        System.out.println(value1 <<= value2);
-        System.out.println(value1 >>= value2);
-        System.out.println(value1 >>>= value2);
+        int abs = (value1 > 9) ? value1 * 10 : value1 * 100;
+        System.out.println(abs);
+
+        System.out.println(((value1 > 9) ? "It is true": "it is false"));
     }
 }
