a40b23e25a7c187b2ee0c402afb8c70dd84e8a39
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-23 09:28:51 +0100

    De ++ operator on a char

--- src/be/intecbrussel/exercise05_01/DataTypesApp.java
+++ src/be/intecbrussel/exercise05_01/DataTypesApp.java
@@ -4,14 +4,9 @@ public class DataTypesApp {
 
     public static void main(String[] args) {
 
-        int a = 5;
-        int b = ++a;
-        int c = 7;
-        int d = c++;
+        char x = 'y';
+        x++;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
+        System.out.println(x);
     }
 }
