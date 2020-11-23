525a7d819a5af48cea385a143705a24f8ff51d6a
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-23 11:06:20 +0100

    Toekenningsoperatoren p72

--- src/be/intecbrussel/exercise05_04/Arithmetic.java
+++ src/be/intecbrussel/exercise05_04/Arithmetic.java
@@ -4,14 +4,20 @@ public class Arithmetic {
 
     public static void main(String[] args) {
 
-        boolean value1 = 3 < 5;
-        boolean value2 = 5 < 3;
+        int value1 = 5;
+        int value2 = 3;
 
-        System.out.println(value1);
-        System.out.println(value2);
-        System.out.println(value1 && value2);
-        System.out.println(value1 || value2);
-        System.out.println(!value1);
-        System.out.println(!value2);
+        System.out.println(value1 = value2);
+        System.out.println(value1 += value2);
+        System.out.println(value1 -= value2);
+        System.out.println(value1 *= value2);
+        System.out.println(value1 /= value2);
+        System.out.println(value1 %= value2);
+        System.out.println(value1 &= value2);
+        System.out.println(value1 |= value2);
+        System.out.println(value1 ^= value2);
+        System.out.println(value1 <<= value2);
+        System.out.println(value1 >>= value2);
+        System.out.println(value1 >>>= value2);
     }
 }
