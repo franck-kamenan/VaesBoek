4e0ce628c812f43ab98034f9d24cdfe03e45af81
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-23 10:37:21 +0100

    Arithmetic p61

---
+++ src/be/intecbrussel/exercise05_04/Arithmetic.java
@@ -0,0 +1,22 @@
+package be.intecbrussel.exercise05_04;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+        int number1 = 5;
+        int number2 = 3;
+        char letterA = 'A';
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 % number2);
+        System.out.println(--number1);
+        System.out.println(number1--);
+        System.out.println(++number1);
+        System.out.println(number1++);
+        System.out.println(++letterA);
+    }
+}
