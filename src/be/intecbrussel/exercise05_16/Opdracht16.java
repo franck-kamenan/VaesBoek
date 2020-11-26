e7e9802476cbd77bbef36da24ee62aac9b1b6383
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-26 14:46:07 +0100

    Opdracht16 p93

---
+++ src/be/intecbrussel/exercise05_16/Opdracht16.java
@@ -0,0 +1,15 @@
+package be.intecbrussel.exercise05_16;
+
+public class Opdracht16 {
+
+    public static void main(String[] args) {
+
+        int count = 121;
+        char letter = 'A';
+
+        while (count > 100) {
+
+            System.out.println(--count);
+        }
+    }
+}
