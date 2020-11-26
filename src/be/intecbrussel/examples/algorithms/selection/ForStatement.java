71d412466cc6cd6ac2f8c02c5c6ae46bef2caf6a
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-26 14:55:04 +0100

    for p.95

---
+++ src/be/intecbrussel/examples/algorithms/selection/ForStatement.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.examples.algorithms.selection;
+
+public class ForStatement {
+
+    public static void main(String[] args) {
+
+        for (int number = 1; number < 10; number++) {
+
+            System.out.println(number);
+        }
+    }
+}
