ad49d9fae659813dfdfd54e501749c8bfd72e2c5
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-26 12:22:36 +0100

    switch fall through p87

--- src/be/intecbrussel/examples/algorithms/selection/DaysInMonthSwitch.java
+++ src/be/intecbrussel/examples/algorithms/selection/DaysInMonthSwitch.java
@@ -14,19 +14,19 @@ public class DaysInMonthSwitch {
 
         switch (month) {
 
-            case 1: days = 31; break;
+            default: days = 0; break;
             case 2: days = 28; break;
-            case 3: days = 31; break;
-            case 4: days = 30; break;
-            case 5: days = 31; break;
-            case 6: days = 30; break;
-            case 7: days = 31; break;
-            case 8: days = 31; break;
-            case 9: days = 30; break;
-            case 10: days = 31; break;
+            case 4:
+            case 6:
+            case 9:
             case 11: days = 30; break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
             case 12: days = 31; break;
-            default: days = 0;
             }
 
         System.out.println("Number of days in month: " + days);
