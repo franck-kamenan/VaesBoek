a1773d40967d1b222554b7cd257eceea0334f862
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-27 12:38:59 +0100

    method name overloading p100

--- src/be/intecbrussel/examples/algorithms/sequence/Methods.java
+++ src/be/intecbrussel/examples/algorithms/sequence/Methods.java
@@ -10,6 +10,7 @@ public class Methods {
 
         System.out.println(c);
         System.out.println(sum(a, b));
+        System.out.println(sum(a, b, c));
         System.out.println("\n");
 
         int d = 7;
@@ -18,21 +19,36 @@ public class Methods {
 
         System.out.println(f);
         System.out.println(sum(d, e));
+        System.out.println(sum(d, e, f));
         System.out.println("\n");
 
         c = sum(a, b);
         f = sum(d, e);
+        int g = sum(c, f);
+        int h = sum(c, f, g);
 
         System.out.println(c);
         System.out.println(f);
-        System.out.println("\n");
-
+        System.out.println(g);
+        System.out.println(h);
         System.out.println("\n");
     }
 
     public static int sum(int number1, int number2){
 
         int result = number1 + number2;
+        return result;
+    }
+
+    public static int sum(int number1, int number2, int number3){
+
+        int result = number1 + number2 + number3;
+        return result;
+    }
+
+    public static double sum(double number1, double number2){
+
+        double result = number1 + number2;
         return result;
     }
 }
