f10a67eda0874a8d1b7d195574a38aa50c09e2fa
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-29 10:50:25 +0100

    Getallen Bij Een String Optellen p130

---
+++ src/be/intecbrussel/hoofdstuk6/GetallenBijEenStringOptellen.java
@@ -0,0 +1,9 @@
+package be.intecbrussel.hoofdstuk6;
+
+public class GetallenBijEenStringOptellen {
+
+    public static void main(String[] args) {
+
+        System.out.println(3 + 7 + " Hello World " + 3 + 7);
+    }
+}
