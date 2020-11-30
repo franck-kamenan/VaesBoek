5a5bb7baebae770fb40504303aa4b6fad1cea463
Franck Kamenan <franck.kamenan@yahoo.com>
2020-11-30 11:59:59 +0100

    Opdracht1 p138

---
+++ src/be/intecbrussel/hoofdstuk7/Opdracht1.java
@@ -0,0 +1,19 @@
+package be.intecbrussel.hoofdstuk7;
+
+public class Opdracht1 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {7, 14, 21, 28};
+
+        for (int i: numbers) {
+
+            System.out.println(i);
+        }
+
+        for (int i = 3; i >= 0 ; i--) {
+
+            System.out.println(numbers[i]);
+        }
+    }
+}
