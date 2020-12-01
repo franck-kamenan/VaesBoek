70db0cef71ab955b0006e3c7185b1d5df8e5b158
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-01 13:00:53 +0100

    Opdracht4 p156

--- src/be/intecbrussel/hoofdstuk8/Rectangle.java
+++ src/be/intecbrussel/hoofdstuk8/Rectangle.java
@@ -6,4 +6,30 @@ public class Rectangle {
     public int y;
     public int height;
     public int width;
+
+    public void setPosition(int xpos, int ypos){
+
+        this.x = xpos;
+        this.y = ypos;
+    }
+
+    public void setHeight(int height) {
+
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+
+        this.width = width;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
 }
