712303d3b2bafc867d5f4de095275e7568fe3f0d
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-05 11:49:42 +0100

    converting Integer to int and int to Integer with valueof and intValue p214

--- src/be/intecbrussel/hoofdstuk12/ClassWrapper.java
+++ src/be/intecbrussel/hoofdstuk12/ClassWrapper.java
@@ -1,18 +1,32 @@
 package be.intecbrussel.hoofdstuk12;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class ClassWrapper {
 
-    public Byte aByteObject;
-    public Short aShortObject;
-    public Integer integerObject;
-    public Long aLongObject;
-    public Float aFloatObject;
-    public Double aDoubleObject;
-    public BigInteger bigIntegerObject;
-    public BigDecimal bigDecimalObject;
-    public Boolean aBooleanObject;
-    public Character characterObject;
+    public static void main(String[] args) {
+
+        Byte aByteObject;
+        Short aShortObject;
+        Integer integerObject;
+        Long aLongObject;
+        Float aFloatObject;
+        Double aDoubleObject;
+        BigInteger bigIntegerObject;
+        BigDecimal bigDecimalObject;
+        Boolean aBooleanObject;
+        Character characterObject;
+
+        Integer intObject = Integer.valueOf(5);
+        System.out.println(intObject);
+        int intPrimitive = intObject.intValue();
+        System.out.println(intPrimitive);
+        Integer secondIntObject = Integer.valueOf(intPrimitive);
+        System.out.println(secondIntObject);
+        int secondIntPrimitive = secondIntObject.intValue();
+        System.out.println(secondIntPrimitive);
+    }
 }
