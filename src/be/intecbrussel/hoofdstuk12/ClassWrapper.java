2abd2fedccb7ee391bd063e2121812a4f9128c8b
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-05 11:33:13 +0100

    wrapper class eenvoudige klassen p214

---
+++ src/be/intecbrussel/hoofdstuk12/ClassWrapper.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.hoofdstuk12;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class ClassWrapper {
+
+    public Byte aByteObject;
+    public Short aShortObject;
+    public Integer integerObject;
+    public Long aLongObject;
+    public Float aFloatObject;
+    public Double aDoubleObject;
+    public BigInteger bigIntegerObject;
+    public BigDecimal bigDecimalObject;
+    public Boolean aBooleanObject;
+    public Character characterObject;
+}
