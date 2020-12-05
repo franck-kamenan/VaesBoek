a12fc2b64dddc4c8d8eb3860348be4cfad5ecf24
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-05 12:45:48 +0100

    LocalDateTime p223

--- src/be/intecbrussel/hoofdstuk12/InstantApp.java
+++ src/be/intecbrussel/hoofdstuk12/InstantApp.java
@@ -1,6 +1,9 @@
 package be.intecbrussel.hoofdstuk12;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class InstantApp {
 
@@ -14,5 +17,19 @@ public class InstantApp {
         System.out.println(later);
         System.out.println(now.isAfter(earlier));
         System.out.println(now.isBefore(later));
+
+        LocalDate nowDate = LocalDate.now();
+        LocalTime nowTime = LocalTime.now();
+        LocalDateTime nowDateTime = LocalDateTime.now();
+        System.out.println(nowDate);
+        System.out.println(nowTime);
+        System.out.println(nowDateTime);
+
+        LocalDate otherDate = LocalDate.of(2020, 12, 4);
+        LocalTime otherTime = LocalTime.of(12, 40, 33);
+        LocalDateTime otherDateTime = LocalDateTime.of(otherDate, otherTime);
+        System.out.println(otherDate);
+        System.out.println(otherTime);
+        System.out.println(otherDateTime);
     }
 }
