8190f7403b230c1ba540437e40bbeb95a34e36b7
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-05 09:30:48 +0100

    instanceof abstract super class p199

--- src/be/intecbrussel/hoofdstuk10/AnimalApp.java
+++ src/be/intecbrussel/hoofdstuk10/AnimalApp.java
@@ -7,10 +7,22 @@ public class AnimalApp {
         Animal myCat = new Cat();
         Animal mySnake = new Snake();
 
+        Cat secondCat = new Cat();
+        Snake secondSnake = new Snake();
+
         myCat.move();
         myCat.makeNoise();
+        secondCat.move();
+        secondCat.makeNoise();
 
         mySnake.move();
         mySnake.makeNoise();
+        secondSnake.move();
+        secondSnake.makeNoise();
+
+        System.out.println(myCat instanceof Animal);
+        System.out.println(secondCat instanceof Animal);
+        System.out.println(mySnake instanceof Animal);
+        System.out.println(secondSnake instanceof Animal);
     }
 }
