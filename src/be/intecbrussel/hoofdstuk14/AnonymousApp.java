eae3a71a27678541fd4d1ac416a53e315fef3615
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-06 12:05:50 +0100

    Test p244

---
+++ src/be/intecbrussel/hoofdstuk14/AnonymousApp.java
@@ -0,0 +1,10 @@
+package be.intecbrussel.hoofdstuk14;
+
+public class AnonymousApp {
+
+    public static void main(String[] args) {
+
+        AnonymousOuterClass anonymousOuterClassObject = new AnonymousOuterClass();
+        anonymousOuterClassObject.method();
+    }
+}
