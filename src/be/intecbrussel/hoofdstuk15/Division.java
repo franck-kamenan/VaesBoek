73f9a4fa2409fdaa85b3d0aad21f7c9b80218f7a
Franck Kamenan <franck.kamenan@yahoo.com>
2020-12-07 09:31:51 +0100

    Exeption p252

---
+++ src/be/intecbrussel/hoofdstuk15/Division.java
@@ -0,0 +1,22 @@
+package be.intecbrussel.hoofdstuk15;
+
+import java.util.Scanner;
+
+public class Division {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Scanner keyboard = new Scanner(System.in);
+            int num = Integer.parseInt(keyboard.next());
+            int den = Integer.parseInt(keyboard.next());
+            int div = num / den;
+            System.out.format("%d / %d= %d", num, den, div);
+            keyboard.close();
+        } catch (NumberFormatException nfe) {
+
+            System.out.println("Invalid number");
+        }
+    }
+}
